b41c29af55c63561335ad9ca628ea0d6bb30db11
Moritz Müller <moritzm.mueller@posteo.de>
2020-10-04 20:40:42 +0200

    added tests

--- pkg/shamir/shamirSplit_test.go
+++ pkg/shamir/shamirSplit_test.go
@@ -5,13 +5,30 @@ import (
 	"testing"
 )
 
-// TestSplitSecret
 func TestSplitSecret(t *testing.T) {
+	{
+		secret := "very secret secret"
+		var threshold uint8 = 3
+		numberOfShares := 5
+		shares, err := SplitSecret([]byte(secret), numberOfShares, threshold)
 
-	shares := SplitSecret([]byte("secret"), 10, 3)
+		assert.Nil(t, err)
 
-	for _, share := range shares {
-		assert.GreaterOrEqual(t, share.shareIndex, 1)
+		assert.Equal(t, len(shares), numberOfShares)
+
+		uuid := shares[0].id
+		for _, share := range shares {
+			assert.Equal(t, uuid, share.id, "All shares should have the same id")
+		}
+
+		for _, share := range shares {
+			assert.Equal(t, threshold, share.threshold, "Al Shares should have the same threshold")
+		}
+
+		len_share := len(shares[0].slices)
+		for _, share := range shares {
+			assert.Equal(t, len_share, len(share.slices), "All shares should have the same length")
+		}
+
 	}
-
 }
