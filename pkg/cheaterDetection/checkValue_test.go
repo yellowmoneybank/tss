d03f8b6e5705cd0292ffccdd53159bc018f38662
Moritz Müller <moritzm.mueller@posteo.de>
2020-10-09 20:23:57 +0200

    removed boilerplate

--- pkg/cheaterDetection/checkValue_test.go
+++ pkg/cheaterDetection/checkValue_test.go
@@ -3,7 +3,6 @@ package cheaterDetection
 import (
 	"fmt"
 	"math/big"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,33 +27,6 @@ func Test_CalculateCheckValue(t *testing.T) {
 	}
 }
 
-func Test_calcT(t *testing.T) {
-	type args struct {
-		shares [][]byte
-		prime  *big.Int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *big.Int
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := calcT(tt.args.shares, tt.args.prime)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("calcT() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("calcT() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_calcSum1(t *testing.T) {
 	assert.NotEmpty(t, calcSum1([][]byte{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}}, big.NewInt(13)))
 }
@@ -62,25 +34,5 @@ func Test_calcSum1(t *testing.T) {
 func Test_calcSum2(t *testing.T) {
 	{
 		assert.Equal(t, int64(1050), calcSum2(big.NewInt(3), big.NewInt(7), 3).Int64())
-	}
-}
-
-func Test_decodeSecret(t *testing.T) {
-	type args struct {
-		share shamir.Share
-	}
-	tests := []struct {
-		name string
-		args args
-		want []byte
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := decodeSecret(tt.args.share); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("decodeSecret() = %v, want %v", got, tt.want)
-			}
-		})
 	}
 }
